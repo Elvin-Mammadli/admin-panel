60ba273a04f9e7531b8a85d35273c3dc7854d2cf
Elvin Mammadli <mamedli.elvin@gmail.com>
2022-05-14 16:19:34 +0400

    color fix of plus sign

--- src/components/ScreenParts/Sidebar.jsx
+++ src/components/ScreenParts/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Avatar, Box, List, SpeedDial as MuiSpeedDial, MenuItem, Menu as MuiMenu, SpeedDialAction, SpeedDialIcon, Typography, IconButton } from '@mui/material'
-import { WorkOutline, InboxOutlined, ChatOutlined, AccessTimeOutlined, EventOutlined, SettingsOutlined, FileCopy } from "@mui/icons-material";
+import { WorkOutline, InboxOutlined, ChatOutlined, AccessTimeOutlined, EventOutlined, SettingsOutlined, FileCopy, Add } from "@mui/icons-material";
 import { Menu, Favorites } from './components';
 
 const actions = [
@@ -10,8 +10,8 @@ const actions = [
 
 const menus = [
   { title: "My Tasks", route: "/mytasks", icon: <InboxOutlined />, id: 1, notification: 0 },
-  { title: "Inbox", route: "/inbox", icon: <ChatOutlined />, id: 2, notification: 0},
-  { title: "Projects", route: "/projects", icon: <WorkOutline />, id:3, notification: 0 },
+  { title: "Inbox", route: "/inbox", icon: <ChatOutlined />, id: 2, notification: 0 },
+  { title: "Projects", route: "/projects", icon: <WorkOutline />, id: 3, notification: 0 },
   { title: "Standups", route: "/standups", icon: <AccessTimeOutlined />, id: 4, notification: 0 },
   { title: "Meetings", route: "/meetings", icon: <EventOutlined />, id: 5, notification: 5 },
   { title: "Settings", route: "/settings", icon: <SettingsOutlined />, id: 6, notification: 0 }
@@ -42,7 +42,7 @@ export const Sidebar = () => {
     <Box bgcolor="#f8f8f8" display="flex" flex={2} flexDirection="column" justifyContent="space-evenly">
       <Box display="flex" alignItems="center" pl={1} pt={2}>
         <IconButton onClick={handleClick}>
-          <Avatar src="https://randomuser.me/api/portraits/men/29.jpg"/>
+          <Avatar src="https://randomuser.me/api/portraits/men/29.jpg" />
         </IconButton>
         <MuiMenu
           anchorEl={anchorEl}
@@ -78,29 +78,9 @@ export const Sidebar = () => {
         ))}
       </List>
 
-      <Box sx={{ height: "90px", position: "relative" }}>
-        <MuiSpeedDial
-          ariaLabel="SpeedDial basic example"
-          sx={{ position: 'absolute', bottom: 16, left: 16 }}
-          FabProps={{
-            sx: {
-              bgcolor: 'error.main',
-              '&:hover': {
-                bgcolor: 'error.main',
-              }
-            }
-          }}
-          icon={<SpeedDialIcon color='error' />}
-        >
-          {actions.map((action) => (
-            <SpeedDialAction
-              key={action.name}
-              icon={action.icon}
-              tooltipTitle={action.name}
-            />
-          ))}
-        </MuiSpeedDial>
-      </Box>
+        <IconButton sx={{ width: "40px", height: "40px", bgcolor: "#ff3e8e", borderRadius: "50%", ml: 2, "&:hover": {bgcolor: "#d42b71"}, color: "#fff" }}>
+          <Add />
+        </IconButton>
       <Typography ml="16px" color="#808080">2022 Elvin Mammadli</Typography>
     </Box>
   )
